580f46784cb492b1a3eb3895669193fd830e5ab3
stsh1119 <stshlaptop@gmail.com>
2021-04-30 20:32:26 +0300

    Validation: start_date cannot be earlier, than end_date

--- yalantis/courses/dto.py
+++ yalantis/courses/dto.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 from datetime import datetime
 
 
@@ -7,3 +7,9 @@ class CourseDto(BaseModel):
     start_date: datetime
     end_date: datetime
     lectures_amount: int
+
+    @validator('end_date')
+    def end_date_must_be_later_than_start_date(cls, v, values):
+        if 'start_date' in values and v < values['start_date']:
+            raise ValueError('start_date must be less than end_date')
+        return v
